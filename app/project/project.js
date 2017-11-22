d19d2d3afbfc129accdefd4b398de028087aa050
rohitbegani <rbegani72@gmail.com>
2017-11-21 20:42:27 -0500

    Old static content

--- app/project/project.js
+++ app/project/project.js
@@ -10,11 +10,15 @@ angular.module('translateam.project', ['ngRoute'])
   });
 }])
 
-.controller('ProjectCtrl', ['$resource', '$location', function($resource, $location) {
+.controller('ProjectCtrl', ['$resource', '$location', function($resource, $location, $scope) {
   var self = this;
+  var todoArray = [];
+  var transcribeArray = [];
+  var translateArray = [];
+  var doneArray = [];
   var Project = $resource('/projects/:projectId');
   var Scenes = $resource('/scenes/:sceneId');
-  var ProjectScenes = $resource('https://localhost:3000/projects/:projectId/scenes');
+  var ProjectScenes = $resource('/scenes/:sceneId');
   var SceneStageUpdate = $resource('http://localhost:3000/scenes/:sceneId',
     null,
     {
@@ -22,6 +26,7 @@ angular.module('translateam.project', ['ngRoute'])
     });
 
   var projectId = $location.search().project || 1;
+  var staticSceneIds = [1, 2, 3, 4];
 
   Project.get({projectId: projectId}).$promise.then(function(project) {
     if(project) {
@@ -34,81 +39,35 @@ angular.module('translateam.project', ['ngRoute'])
       self.scenes = project.scenes;
       console.log(self.scenes)
     }
-  })
-
-  /**
-   * Function to update the Project state of a selected scene
-   * */
-  // ProjectScenes.query({projectId: projectId}).$promise.then(function(status) {
-  //
-  // })
-
-
-  /**
-   * JS code for demo, needs to be worked upon
-   * TODO: Update the code to work for our situation
-   * */
-
-  $scope.models = [
-    {listName: "A", items: [], dragging: false},
-    {listName: "B", items: [], dragging: false}
-  ];
-
-  /**
-   * dnd-dragging determines what data gets serialized and send to the receiver
-   * of the drop. While we usually just send a single object, we send the array
-   * of all selected items here.
-   */
-  $scope.getSelectedItemsIncluding = function(list, item) {
-    item.selected = true;
-    return list.items.filter(function(item) { return item.selected; });
-  };
-
-  /**
-   * We set the list into dragging state, meaning the items that are being
-   * dragged are hidden. We also use the HTML5 API directly to set a custom
-   * image, since otherwise only the one item that the user actually dragged
-   * would be shown as drag image.
-   */
-  $scope.onDragstart = function(list, event) {
-    list.dragging = true;
-    if (event.dataTransfer.setDragImage) {
-      var img = new Image();
-      img.src = 'framework/vendor/ic_content_copy_black_24dp_2x.png';
-      event.dataTransfer.setDragImage(img, 0, 0);
-    }
-  };
-
-  /**
-   * In the dnd-drop callback, we now have to handle the data array that we
-   * sent above. We handle the insertion into the list ourselves. By returning
-   * true, the dnd-list directive won't do the insertion itself.
-   */
-  $scope.onDrop = function(list, items, index) {
-    angular.forEach(items, function(item) { item.selected = false; });
-    list.items = list.items.slice(0, index)
-      .concat(items)
-      .concat(list.items.slice(index));
-    return true;
-  }
-
-  /**
-   * Last but not least, we have to remove the previously dragged items in the
-   * dnd-moved callback.
-   */
-  $scope.onMoved = function(list) {
-    list.items = list.items.filter(function(item) { return !item.selected; });
-  };
-
-  // Generate the initial model
-  angular.forEach($scope.models, function(list) {
-    for (var i = 1; i <= 4; ++i) {
-      list.items.push({label: "Item " + list.listName + i});
-    }
   });
 
-  // Model to JSON for demo purpose
-  $scope.$watch('models', function(model) {
-    $scope.modelAsJson = angular.toJson(model, true);
-  }, true);
+  staticSceneIds.map( function (s) {
+    ProjectScenes.get({sceneId: s}).$promise.then(function (scenes) {
+      if (scenes) {
+        self.scenes = scenes;
+        self.sortIntoArrays(self.scenes);
+      }
+    })
+  });
+
+  self.sortIntoArrays = function(s) {
+    if (s.projectStage === 'todo') {
+      todoArray.push(s);
+      console.log(s.projectStage);
+      console.log(todoArray);
+    } else if (s.projectStage === 'transcribe') {
+      transcribeArray.push(s);
+      console.log(s.projectStage);
+      console.log(transcribeArray);
+    } else if (s.projectStage === 'translate') {
+      translateArray.push(s);
+      console.log(s.projectStage);
+      console.log(translateArray);
+    } else if (s.projectStage === 'done') {
+      doneArray.push(s);
+      console.log(s.projectStage);
+      console.log(doneArray);
+    }
+  };
+
 }]);
